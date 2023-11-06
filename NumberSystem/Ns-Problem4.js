19288ac40f75665905b4cbe4b5cd48ef22f15ca9
Abhishek Pratap Singh <abhisi764@gmail.com>
2023-11-06 15:30:36 +0530

    Problem 4 Even Or Odd

---
+++ NumberSystem/Ns-Problem4.js
@@ -0,0 +1,11 @@
+// Q. Find out given number is an even or odd? 
+
+function evenOrOdd(num) {
+    if (num % 2 == 0) {
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
+let number = 4;
+console.log(evenOrOdd(number)) //Even
