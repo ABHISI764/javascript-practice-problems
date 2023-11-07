e45eba77cd56521283772863e67ba5b62886a750
Abhishek Pratap Singh <abhisi764@gmail.com>
2023-11-07 13:47:52 +0530

    Ns-Problem6 using while

--- NumberSystem/Ns-Problem6.js
+++ NumberSystem/Ns-Problem6.js
@@ -9,3 +9,18 @@ function alloddNumber(start, end) {
 const start = 1;
 const end = 10;
 alloddNumber(start, end)
+
+// same result using while 
+
+// function oddNumbers(start, end){
+//     let i = start;
+//     while(i <= end){
+//         if(i % 2 !== 0 ){
+//             console.log(i)
+//         }
+//         i++
+//     }
+// }
+// const start = 1;
+// const end = 10;
+// oddNumbers(start, end) // 1 3 5 7 9
