17f4f5b13572354e113d13c9d15569c5cc3e7f45
Abhishek Pratap Singh <abhisi764@gmail.com>
2023-11-08 19:05:32 +0530

    Ns-Problem9 Swap Two variable without 3rd

---
+++ NumberSystem/Ns-Problem9.js
@@ -0,0 +1,11 @@
+// Q. Swap two int variables without 3rd variable?
+
+let a = 5;
+let b = 10;
+console.log("Before Swapping");
+console.log("a =", a);
+console.log("b =", b);
+[a, b] = [b, a];
+console.log("After Swapping");
+console.log("a =", a);
+console.log("b =", b)
