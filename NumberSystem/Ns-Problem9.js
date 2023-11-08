9f5478b663daece92b07e69ca6034fd43bbab55f
Abhishek Pratap Singh <abhisi764@gmail.com>
2023-11-08 19:26:56 +0530

    Ns-Problem9 same result using addition

--- NumberSystem/Ns-Problem9.js
+++ NumberSystem/Ns-Problem9.js
@@ -9,3 +9,21 @@ console.log("b =", b);
 console.log("After Swapping");
 console.log("a =", a);
 console.log("b =", b)
+
+
+// same result using Addition and Subtraction 
+
+// let a = 5;
+// let b = 10;
+
+// console.log("Before swapping:");
+// console.log("a =", a);
+// console.log("b =", b);
+
+// a = a + b; // (5 + 10) a= 15; 
+// b = a - b; // (15 - 10) b = 5;
+// a = a - b; // (15 - 5) a = 10
+
+// console.log("After swapping:");
+// console.log("a =", a);
+// console.log("b =", b);
