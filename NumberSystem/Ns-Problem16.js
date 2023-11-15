04ae9caa7ecd990a35d01c7f70b2213855a85a13
Abhishek Pratap Singh <abhisi764@gmail.com>
2023-11-15 10:00:56 +0530

    factorial same result using Iterative Approach

--- NumberSystem/Ns-Problem16.js
+++ NumberSystem/Ns-Problem16.js
@@ -12,3 +12,17 @@ function factorial(n) {
 let number = 5;
 let result = factorial(number)
 console.log(result) // 120
+
+// Method 2 - Iterative Approach:
+
+// function factorial(num) {
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// let number = 5;
+// let result = factorial(number)
+// console.log(result) // 120
