6c834fbb99c2c79f2d859d8553bde87d715c59bf
Abhishek Pratap Singh <abhisi764@gmail.com>
2023-11-25 05:35:19 +0530

    find average value using Loop

--- Arrays/Arrays-Problem6.js
+++ Arrays/Arrays-Problem6.js
@@ -8,7 +8,7 @@ const myArray = [5, 2, 8, 1, 7];
 let result = calculateAvrage(myArray);
 console.log(result) // 4.6
 
-// Using the forEach() Method
+// Method 2 : Using the forEach() Method
 
 // calculateAvrage = (arr) =>{
 //     let sum = 0;
@@ -17,9 +17,23 @@ console.log(result) // 4.6
 //     });
 //     let average = sum / arr.length;
 //     return average;
-    
 // }
 
 // const myArray = [5, 2, 8, 1, 7];
 // let result = calculateAvrage(myArray);
 // console.log(result)
+
+// Method 3: Using a For Loop
+
+// function calculateAvrage(arr){
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     let average = sum / arr.length
+//     return average;
+// }
+
+// const myArray = [5, 2, 8, 1, 7];
+// let result = calculateAvrage(myArray);
+// console.log(result) // 4.6
