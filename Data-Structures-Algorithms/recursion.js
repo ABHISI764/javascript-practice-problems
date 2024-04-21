7d39a13e9c9de897a0fea65ee2370d556872f12b
Abhishek Pratap Singh <abhisi764@gmail.com>
2024-04-22 01:20:08 +0530

    factorial using recursion

--- Data-Structures-Algorithms/recursion.js
+++ Data-Structures-Algorithms/recursion.js
@@ -21,3 +21,13 @@ countDown(5)
 //     console.log("All done")
 // }
 // countDown(5)
+
+// Method 3 Factorial Calculation 
+
+function factorial(num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+console.log(factorial(5)) // outpur 120 
