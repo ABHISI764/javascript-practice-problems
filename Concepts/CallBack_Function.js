4364f25ef5b29470c1fe4ceab11caca72a07d6ca
Abhishek Pratap Singh <abhisi764@gmail.com>
2024-05-01 11:19:25 +0530

    callback function

--- Concepts/CallBack_Function.js
+++ Concepts/CallBack_Function.js
@@ -4,6 +4,7 @@
  This function that is passed as an argument inside of another function
   is called a callback function. */
 
+// Example 1: 
 sum(display, 5, 4);
 function sum(callback, x, y) {
     let result = x + y;
@@ -13,3 +14,31 @@ function sum(callback, x, y) {
 function display(result) {
     console.log(result)
 }
+
+
+// Example 2 : 
+
+function performOperations(x,y,callback1,callback2,callback3){
+    let sum = x + y;
+    let subtraction	 = x - y;
+    let multiplication = x * y;
+    callback1(sum);
+    callback2(subtraction);
+    callback3(multiplication);
+}
+function handelSum(result){
+    console.log("Sum",result)
+}
+
+function handelSubtraction(result){
+        console.log("Subtraction",result)    
+}
+
+function handelmultiplication(result){
+    console.log("Multiplication", result)
+}
+
+performOperations(5,3,handelSum,handelSubtraction,handelmultiplication)
+//Sum 8
+//Subtraction 2
+//Multiplication 15
