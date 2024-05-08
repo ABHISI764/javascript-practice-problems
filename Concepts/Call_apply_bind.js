ecf06d76ee3b5d73c850025d38d0b6d7ef7904fa
Abhishek Pratap Singh <abhisi764@gmail.com>
2024-05-08 22:19:40 +0530

    Call aplly bind

---
+++ Concepts/Call_apply_bind.js
@@ -0,0 +1,28 @@
+// Q. what is call apply bind 
+/* Ans. 
+call  :-  using call we can borrow function from another object.
+Apply :- aplly same like call only deffrence is we pass argument as array
+bind : bind is same diffrence is bind retrun new function and arguments bound to it
+Video URL :- https://www.youtube.com/watch?v=75W8UPQ5l7k&t=511s
+*/
+
+let name = {
+    firstName: "Akshay",
+    lastNmae: "Saini",
+}
+let printFullName = function (homeTown, state) {
+    console.log(this.firstName + " " + this.lastNmae + " From " + homeTown + ", " + state)
+} 
+
+let name2 = { 
+    firstName: "Sachin ",
+    lastNmae: "Tendulkar",
+}
+
+// function borrowing
+printFullName.call(name2, "Mumbai", "Maharastra");
+// apply method
+printFullName.apply(name2, ["Mumbai", "Maharastra"]);
+// bind method
+let printMyName = printFullName.bind(name2, "Mumbai", "Maharastra")
+console.log(printMyName())
