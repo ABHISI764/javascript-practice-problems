a416457fa2347ade2f8c8907b1f25ab2d5d512f4
Abhishek pratap Singh <abhishekpratapsingh@Abhisheks-MacBook-Pro.local>
2024-07-22 20:06:07 +0530

    includes method

---
+++ JavaScript-Array-Methods/includes_array.js
@@ -0,0 +1,16 @@
+/*The includes() method of Array instances determines whether an array includes
+ a certain value among its entries, returning true or false as appropriate. */
+
+/*The includes() method returns true if a string contains a specified string.
+Otherwise it returns false. */
+
+ // Method 1:- 
+
+ const pets = ['cat', 'dog', 'bat'];
+console.log(pets.includes('cat'));
+
+// Method 2:- 
+
+let text = "Hello world, welcome to the universe.";
+let result = text.includes("world");
+console.log(result)
