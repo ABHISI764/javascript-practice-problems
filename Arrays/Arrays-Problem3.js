6c2e30ae783035bf0c050a57be1344025c766bac
Abhishek Pratap Singh <abhisi764@gmail.com>
2025-07-24 21:45:32 +0530

    max number problem

---
+++ Arrays/Arrays-Problem3.js
@@ -0,0 +1,7 @@
+// Q3. Find the max element from the given int array?
+
+// Solution 1: using Math.max
+
+const arr = [10, 25, 7, 89, 45];
+const max = Math.max(...arr);
+console.log("Max number is ",max) // 89
