92c1ecc6e988d0f8de9f73cedc847b16fe9646a9
Abhishek Pratap Singh <abhisi764@gmail.com>
2025-07-25 00:53:36 +0530

    max elemnet using far

--- Arrays/Arrays-Problem3.js
+++ Arrays/Arrays-Problem3.js
@@ -5,3 +5,15 @@
 const arr = [10, 25, 7, 89, 45];
 const max = Math.max(...arr);
 console.log("Max number is ",max) // 89
+
+
+// Solution 2: using for
+
+const arr = [10, 25, 7, 89, 45];
+let max = arr[0]
+for(let i = 0; i < arr.length; i++){
+    if (arr[i] > max) {
+        max = arr[i]; 
+    }
+}
+console.log(max)
