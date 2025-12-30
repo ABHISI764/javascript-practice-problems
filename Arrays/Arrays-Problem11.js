b8b88357aaeddbda4d3a2866470c9521f2fb67bf
Abhishek Pratap Singh <abhisi764@gmail.com>
2025-12-31 02:16:20 +0530

    find endexed element

---
+++ Arrays/Arrays-Problem11.js
@@ -0,0 +1,11 @@
+// Q. Find out an index of a specified element from a given array?
+
+function findIndex(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  console.log(findIndex([10, 20, 30, 40, 50], 30));
