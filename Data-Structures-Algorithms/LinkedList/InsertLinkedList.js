0ed627d8f571ed9f38b2cd47fa6cc23d1fb55486
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-01-13 02:49:48 +0530

    Linklist getAt

--- Data-Structures-Algorithms/LinkedList/InsertLinkedList.js
+++ Data-Structures-Algorithms/LinkedList/InsertLinkedList.js
@@ -1,84 +1,100 @@
 class Node {
-    constructor(data, next = null) {
-      this.data = data;
-      this.next = next;
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
   }
-  
-  class Linklist {
-    constructor() {
-      this.head = null;
-      this.size = 0;
-    }
-  
-    // Insert first node
-    insertFirst(data) {
-      this.head = new Node(data, this.head);
-      this.size++;
-    }
-  
-    // Insert last node
-    insertLast(data) {
-      let node = new Node(data);
-      let current;
-      // If empty, make head
-      if (!this.head) {
-        this.head = node;
-      } else {
-        current = this.head;
-        while (current.next) {
-          current = current.next;
-        }
-        current.next = node;
-      }
-      this.size++;
-    }
-  
-    // Insert at index
-    insertAt(data, index) {
-      //  If index is out of range
-      if (index > 0 && index > this.size) {
-        return;
-      }
-  
-      // If first index
-      if (index === 0) {
-        this.insertFirst(data);
-        return;
-      }
-  
-      const node = new Node(data);
-      let current, previous;
-  
-      // Set current to first
+}
+
+class Linklist {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+
+  // Insert last node
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+    // If empty, make head
+    if (!this.head) {
+      this.head = node;
+    } else {
       current = this.head;
-      let count = 0;
-  
-      while (count < index) {
-        previous = current; // Node before index
-        count++;
-        current = current.next; // Node after index
-      }
-  
-      node.next = current;
-      previous.next = node;
-      this.size++;
-    }
-  
-    printListData() {
-      let current = this.head;
-      while (current) {
-        console.log(current.data);
+      while (current.next) {
         current = current.next;
       }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  // Insert at index
+  insertAt(data, index) {
+    //  If index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    // If first index
+    if (index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current; // Node before index
+      count++;
+      current = current.next; // Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+    this.size++;
+  }
+
+    // Get at index
+    getAt(index){
+      let current = this.head;
+      let count = 0;
+      while(current){
+        if(count == index){
+          console.log(current.data);
+        }
+        count++;
+        current = current.next;
+      }
+      return null;
+    }
+
+
+  printListData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
     }
   }
-  
-  const ll = new Linklist();
-  ll.insertFirst(100);
-  ll.insertFirst(200);
-  ll.insertFirst(300);
-  ll.insertLast(400);
-  ll.insertAt(500, 2);
-  ll.printListData();
-  
+}
+
+const ll = new Linklist();
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400);
+//   ll.insertAt(500, 2);
+ll.getAt(2);
+//   ll.printListData();
+
