dcd61ac7e6430cd3c3c6837cd500aa450f7861bd
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-01-15 12:50:06 +0530

    remove dublicated from array

---
+++ Arrays/Remove-Duplicates-from-Sorted-Array.js
@@ -0,0 +1,13 @@
+// Q. Remove Duplicates from Sorted Array
+
+var removeDuplicates = function(nums) {
+    let withoutDublicate = [];
+    for (let i = 1; i < nums.length; i++) {
+      if (withoutDublicate.indexOf(nums[i]) === -1) {
+          withoutDublicate.push(nums[i])
+      }
+    }
+    return withoutDublicate;
+  }
+  let nums = [1,1,2];
+  console.log(removeDuplicates(nums));
