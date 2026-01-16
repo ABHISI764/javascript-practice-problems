469a97cf641a13570c5f243eca45f8a370afabe6
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-01-16 15:51:03 +0530

    output

--- Arrays/Remove-Duplicates-from-Sorted-Array.js
+++ Arrays/Remove-Duplicates-from-Sorted-Array.js
@@ -10,4 +10,4 @@ var removeDuplicates = function(nums) {
     return withoutDublicate;
   }
   let nums = [1,1,2];
-  console.log(removeDuplicates(nums));
+  console.log(removeDuplicates(nums));// 1,2
