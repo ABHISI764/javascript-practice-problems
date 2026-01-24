8c567649206500bdb8368bd3711054e5b3c0c0de
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-01-25 01:43:40 +0530

    insertAtEnd

--- Data-Structures-Algorithms/LinkedList/DoublyLinkedList.js
+++ Data-Structures-Algorithms/LinkedList/DoublyLinkedList.js
@@ -14,11 +14,31 @@ class Node {
     insertAtBeginning(data) {
       let newNode = new Node(data);
       if (this.head !== null) {
-        this.head.prev = newNode;
+      console.log("test", this.head.prev = newNode);  
         newNode.next = this.head;
       }
       this.head = newNode;
     }
+
+    insertAtEnd(data) {
+        let newNode = new Node(data);
+        if(this.head === null){
+            this.head = newNode;
+            return;
+        }
+        let temp = this.head;
+        while(temp.next !== null){
+           
+            temp = temp.next;
+            console.log("testing",temp)
+        }
+        console.log("testing",temp)
+
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    
   
     display() {
       let current = this.head;
@@ -36,6 +56,7 @@ class Node {
   }
   
   let dll = new DoublyLinkedList();
-  dll.insertAtBeginning(5);
+  dll.insertAtBeginning(10);
+  dll.insertAtEnd(20);
   dll.display();
   
