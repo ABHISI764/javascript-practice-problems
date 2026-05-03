1d460283aad3823e74e28b043502692c45a0cbec
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-05-04 02:03:59 +0530

    array reverse order

---
+++ Arrays/Arrays-Problem12.js
@@ -0,0 +1,7 @@
+// Q. Read all elements from an array in the reverse order? 
+
+const arr = [1, 2, 3, 4, 5];
+
+const reversed = arr.slice().reverse();
+console.log("input arr", arr);
+console.log("Output arr", reversed);
