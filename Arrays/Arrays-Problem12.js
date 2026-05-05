967c3c015f859bc94ac06abe01825d097dedc9c0
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-05-06 02:50:01 +0530

    reverse array

--- Arrays/Arrays-Problem12.js
+++ Arrays/Arrays-Problem12.js
@@ -5,3 +5,14 @@ const arr = [1, 2, 3, 4, 5];
 const reversed = arr.slice().reverse();
 console.log("input arr", arr);
 console.log("Output arr", reversed);
+
+// Method 2: 
+
+const arr = [1, 2, 3, 4, 5];
+let reverseArr = [];
+
+for(let i = arr.length -1; i >= 0; i--){
+    reverseArr.push(arr[i])
+}
+
+console.log(reverseArr)
