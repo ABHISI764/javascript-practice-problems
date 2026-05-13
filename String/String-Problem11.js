454176712b67bfe3c06ac4536c6d07d6c6c67508
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-05-14 03:44:45 +0530

    Write a javascript program to remove all white spaces from a string?

---
+++ String/String-Problem11.js
@@ -0,0 +1,14 @@
+function removeSpaces(str){
+    let result = ""
+    for(let i = 0; i <str.length;i++){
+        if(str[i] !== " "){
+            result += str[i]
+        }
+    }
+    
+    return result;
+}
+
+const str = "Java Script";
+let outPut = removeSpaces(str)
+console.log(outPut);
