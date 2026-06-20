874a592d0193c787c2d00ebf594ec939a686f7c8
Abhishek Pratap Singh <abhisi764@gmail.com>
2026-06-20 23:58:43 +0530

    stack implementation using javascript

---
+++ Data-Structures-Algorithms/STACKS/stackUsingJavascript.js
@@ -0,0 +1,40 @@
+class Stack {
+    constructor(){
+        this.items = [];
+    }
+    push(element){
+        this.items.push(element);
+    }
+    pop(){
+        if(this.isEmpty()){
+            return null;
+        }
+        return this.items.pop();
+    }
+    peek() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.items[this.items.length - 1];
+    }
+    isEmpty(){
+        return this.items.length === 0;
+    }
+    size(){
+      return this.items.length
+    }
+    print(){
+        console.log("print",this.items)
+    }
+}
+
+const stack = new Stack();
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.pop()
+stack.print()
+console.log(stack.peek()); 
+console.log(stack.pop());
+console.log(stack.isEmpty());
+console.log(stack.size())
